02608c63e639a7ffc493f9b3bc30e3e99382e8e7
igselcuk <i.goktan.selcuk@hotmail.com>
2020-04-27 21:56:57 +0300

    add fire effect and joystick movement

---
+++ Assets/Scripts/DemoARController.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+using UnityEngine.EventSystems;
+#if UNITY_EDITOR
+// Set up touch input propagation while using Instant Preview in the editor.
+using Input = GoogleARCore.InstantPreviewInput;
+#endif
+public class DemoARController : MonoBehaviour
+{
+    public Camera fpsCam;
+    public GameObject firePrefab;
+    public Joystick rightAndForward;
+    public Joystick upAndDown;
+    
+    private Rigidbody rigidbody = null;
+    private float fireSpeed = 0.5f;
+    private GameObject _gameObject;
+    
+    private bool m_IsQuitting = false;
+    private bool canMove = false;
+    
+    public void Awake()
+    {
+        // Enable ARCore to target 60fps camera capture frame rate on supported devices.
+        // Note, Application.targetFrameRate is ignored when QualitySettings.vSyncCount != 0.
+        Application.targetFrameRate = 60;
+    }
+    
+
+    private void Move()
+    {
+        var transform1 = _gameObject.transform;
+        Vector3 movement = (transform1.forward * rightAndForward.Vertical + 
+                            transform1.right * rightAndForward.Horizontal + 
+                            transform1.up * upAndDown.Vertical) * fireSpeed * Time.deltaTime;
+        
+        rigidbody.MovePosition(rigidbody.position + movement);
+    }
+
+    private void FixedUpdate()
+    {
+        if (canMove)
+        {
+            Move();
+        }
+    }
+
+    public void Update()
+    {
+        _UpdateApplicationLifecycle();
+
+        // If the player has not touched the screen, we are done with this update.
+        Touch touch;
+        if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+        {
+            return;
+        }
+
+        // Should not handle input if the player is pointing on UI.
+        if (EventSystem.current.IsPointerOverGameObject(touch.fingerId))
+        {
+            return;
+        }
+
+        // Raycast against the location the player touched to search for planes.
+        TrackableHit hit;
+        TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon |
+            TrackableHitFlags.FeaturePointWithSurfaceNormal;
+
+        if (Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit))
+        {
+            // Use hit pose and camera pose to check if hittest is from the
+            // back of the plane, if it is, no need to create the anchor.
+            if ((hit.Trackable is DetectedPlane) &&
+                Vector3.Dot(fpsCam.transform.position - hit.Pose.position,
+                    hit.Pose.rotation * Vector3.up) < 0)
+            {
+                Debug.Log("Hit at back of the current DetectedPlane");
+            }
+            else
+            {
+                // Choose the prefab based on the Trackable that got hit.
+                GameObject prefab = firePrefab;
+                
+                // Instantiate prefab at the hit pose.
+                var gameObject = Instantiate(prefab, hit.Pose.position, hit.Pose.rotation);
+
+                // Create an anchor to allow ARCore to track the hitpoint as understanding of
+                // the physical world evolves.
+                var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+
+                // Make game object a child of the anchor.
+                gameObject.transform.parent = anchor.transform;
+                _gameObject = anchor.transform.GetChild(0).gameObject;
+                rigidbody = anchor.transform.GetChild(0).gameObject.GetComponent<Rigidbody>();
+                rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+                canMove = true;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Check and update the application lifecycle.
+    /// </summary>
+    private void _UpdateApplicationLifecycle()
+    {
+        // Exit the app when the 'back' button is pressed.
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        // Only allow the screen to sleep when not tracking.
+        if (Session.Status != SessionStatus.Tracking)
+        {
+            Screen.sleepTimeout = SleepTimeout.SystemSetting;
+        }
+        else
+        {
+            Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        }
+
+        if (m_IsQuitting)
+        {
+            return;
+        }
+
+        // Quit if ARCore was unable to connect and give Unity some time for the toast to
+        // appear.
+        if (Session.Status == SessionStatus.ErrorPermissionNotGranted)
+        {
+            _ShowAndroidToastMessage("Camera permission is needed to run this application.");
+            m_IsQuitting = true;
+            Invoke("_DoQuit", 0.5f);
+        }
+        else if (Session.Status.IsError())
+        {
+            _ShowAndroidToastMessage(
+                "ARCore encountered a problem connecting.  Please start the app again.");
+            m_IsQuitting = true;
+            Invoke("_DoQuit", 0.5f);
+        }
+    }
+
+    /// <summary>
+    /// Actually quit the application.
+    /// </summary>
+    private void _DoQuit()
+    {
+        Application.Quit();
+    }
+
+    /// <summary>
+    /// Show an Android toast message.
+    /// </summary>
+    /// <param name="message">Message string to show in the toast.</param>
+    private void _ShowAndroidToastMessage(string message)
+    {
+        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject unityActivity =
+            unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+        if (unityActivity != null)
+        {
+            AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast");
+            unityActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+            {
+                AndroidJavaObject toastObject =
+                    toastClass.CallStatic<AndroidJavaObject>(
+                        "makeText", unityActivity, message, 0);
+                toastObject.Call("show");
+            }));
+        }
+    }
+}
